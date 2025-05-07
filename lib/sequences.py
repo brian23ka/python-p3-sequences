620d02625895922b0575cc4ab30b110754bee5b7
Brian Kaloki <brianngenye@gmail.com>
2025-05-07 09:04:07 +0300

    MY FIRST COMMIT

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    
+    for i in range(length):
+        if i == 0:
+            fibonacci.append(0)
+        elif i == 1:
+            fibonacci.append(1)
+        else:
+            next_number = fibonacci[i - 1] + fibonacci[i - 2]
+            fibonacci.append(next_number)
+    
+    print(fibonacci)
+def print_fibonacci(length):
+    fibonacci = []
+    
+    for i in range(length):
+        if i == 0:
+            fibonacci.append(0)
+        elif i == 1:
+            fibonacci.append(1)
+        else:
+            next_number = fibonacci[i - 1] + fibonacci[i - 2]
+            fibonacci.append(next_number)
+    
+    print(fibonacci)
